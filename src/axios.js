a0b21e95cdd6de2338f50a339c18320fa1dc099e
elipeF <matej6543211@gmail.com>
2020-10-21 20:46:03 +0200

    fix typo

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create();
 
-if (instance.defaults.headers.common["Authorization"]) {
+if (window.localStorage.getItem("jwt_token")) {
   instance.defaults.headers.common["Authorization"] =
     "bearer " + window.localStorage.getItem("jwt_token");
 }
